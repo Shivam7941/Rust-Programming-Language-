20eb80e85ea83a9285f2f8ed025bff3162b6a842
Shivam Pandey <shivam783p@gmail.com>
2026-01-20 20:57:06 +0530

    added info

--- Ch-1/intro.rs
+++ Ch-1/intro.rs
@@ -1,7 +1,16 @@
 // So, this is our first program
 
 
-fn main() {
-    println!("Hello, world!");
+fn main() { //fn stands for function and it's how you define a function in Rust
+
+//main is a special function and the entry point of every Rust program
+    
+    println!("Hello, world!"); //println! - It stands for print line
+    
+    //println! has ! - it's a macro that gets expanded before compilation
+
     println!("This is my first Rust program !");
+
+
+
 }
